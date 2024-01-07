59e64f421cde0742a13e4dbbcadc2f5ebda61db1
JoshWilger <hap3guy@gmail.com>
2024-01-07 16:06:40 -0600

    Fixed user logo not showing when chatting

--- src/components/Main/ModelResponsePanel/index.tsx
+++ src/components/Main/ModelResponsePanel/index.tsx
@@ -3,6 +3,7 @@ import Loader from '@components/common/Loader';
 import { loaderElements } from '@components/common/Loader/constants';
 import Flicker from '@assets/Main/flicker.svg';
 import logo from '@assets/Main/logo-icon.svg';
+import user from '@assets/Main/user.svg';
 import { useFormSelector } from '@store/formContext';
 import useIncrementTimeout from '../hooks/useIncrementTimeout';
 import useTypingEffect from '../hooks/useTypingEffect';
@@ -11,18 +12,20 @@ import useTypingEffect from '../hooks/useTypingEffect';
 const ModelResponsePanel = () => {
   const responsePanelRef = useRef(null);
 
-  const { modelResponse, isLoading } = useFormSelector();
+  const { formValues: { conversationContext }, modelResponse, isLoading } = useFormSelector();
   const { isAnimating } = useTypingEffect(modelResponse[0]);
   const { count } = useIncrementTimeout(isLoading, loaderElements.length);
 
   // useScrollToRef(responsePanelRef, isLoading || modelMessage.length, count);
+  const messages = isLoading ? ["", ...modelResponse] : modelResponse
+  const userMessages = conversationContext.filter(context => context.role === 'user').map(message => message.content)
 
   return (
-    modelResponse.map((message, id) => 
+    messages.map((message, id) => 
     <section className="flex w-[950px] xl:w-[730px] lg:w-[100%] lg:px-[40px] md:px-[20px] sm:px-[10px]" key={id}>
       <img
-        src={logo}
-        alt="Wedding Wordsmith"
+        src={ userMessages.includes(message) ? user : logo }
+        alt={ userMessages.includes(message) ? "User" : "Wedding Wordsmith" }
         className="w-[45px] md:w-[40px] sm:w-[35px] mr-4 md:mr-2 self-start"
       />
       <div className="flex flex-col justify-center w-full min-h-[50px] p-4 sm:p-2 border border-border-default leading-6 rounded-md shadow-sectionInput bg-[#fffcf5]">
