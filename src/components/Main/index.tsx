b4b007d5136cc6fec9ee20ab6c0b5b934fc2766e
JoshWilger <hap3guy@gmail.com>
2024-03-14 12:54:44 -0500

    Fixed wording for how it works

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -12,10 +12,11 @@ const Main = () => {
         <div className="flex flex-col items-center gap-4 sm:gap-10 w-full text-slate-200">
           <h4 className="font-bold text-3xl">How it works</h4>
           <div className="flex flex-col gap-4 px-5 text-left">
-            <p>1. Start the conversation by selecting one of the above prompts</p>
-            <p>2. Answer questions</p>
-            <p>3. Your speech is crafted</p>
-            <p>4. Finalize your speech</p>
+            <p>1. Fill in your OpenAI API Key</p>
+            <p>2. Choose one of two buttons</p>
+            <p>3. Answer the wizard's questions</p>
+            <p>4. Your cover letter is crafted</p>
+            <p>5. Finalize your cover letter</p>
           </div>
           <p className="italic text-xl">It's that simple!</p>
         </div>
